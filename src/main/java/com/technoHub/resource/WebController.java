a00ac82693e58f05c76ac8ab5bc8d81e640d6bd3
github <github@techno-hub.org>
2021-08-19 05:36:48 +0000

    commit

--- src/main/java/com/technoHub/resource/WebController.java
+++ src/main/java/com/technoHub/resource/WebController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -28,6 +29,7 @@ import com.technoHub.service.RequestServices;
 import com.technoHub.service.UserServices;
 
 @Controller
+@CrossOrigin(origins = "https://technomentorship.org" )
 public class WebController {
 
 	@Autowired
